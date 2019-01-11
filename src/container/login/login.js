2b4b6b6251610296440383d3401614d583abc5d3
jonjayang <1010629433@qq.com>
2019-01-11 17:41:27 +0800

    login/register

---
+++ src/container/login/login.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+class Login extends Component{
+    render(){
+        return <h2>登录也</h2>
+    }
+}
+export default Login
---
+++ src/container/register/register.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+class Register extends Component{
+    render(){
+        return <h2>注册页</h2>
+    }
+}
+export default Register
